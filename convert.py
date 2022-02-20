04dd99b2d747c777e73749d6d264b57b256d3a9e
Moshe Bergman <mb-dev@users.noreply.github.com>
2022-02-19 23:18:03 -0800

    link to google maps and skyvector

--- convert.py
+++ convert.py
@@ -1,21 +1,22 @@
 import json
+import urllib.parse as urllib
 import re
+from dms2dec.dms_convert import dms2dec
 from configparser import ConfigParser
 from xml.dom import minidom
 
-username = "Moshe Bergman"
-bush_trip_folder = "asobo-bushtrip-nevada"
-path_to_flt = r"Nevada.FLT"
-path_to_xml = r"nevada.xml"
-company = "Asobo"
-bush_trip_path = r"C:\Users" + '\\' + username + r"\AppData\Roaming\Microsoft Flight Simulator\Packages\Official\Steam" + '\\' + bush_trip_folder + '\\'
-inner_bush_trip_path = bush_trip_path + r"Missions" + '\\' + company + r"\BushTrips\nevada" + '\\'
+path_to_locPak = r"C:\Users\Moshe Bergman\AppData\Roaming\Microsoft Flight Simulator\Packages\Official\Steam\asobo-bushtrip-nevada\en-US.locPak"
+path_to_flt = r"C:\Users\Moshe Bergman\AppData\Roaming\Microsoft Flight Simulator\Packages\Official\Steam\asobo-bushtrip-nevada\Missions\Asobo\BushTrips\nevada\Nevada.FLT"
+path_to_xml = r"C:\Users\Moshe Bergman\AppData\Roaming\Microsoft Flight Simulator\Packages\Official\Steam\asobo-bushtrip-nevada\Missions\Asobo\BushTrips\nevada\nevada.xml"
+image_path_prefix = r"C:\Users\Moshe Bergman\AppData\Roaming\Microsoft Flight Simulator\Packages\Official\Steam\asobo-bushtrip-nevada\Missions\Asobo\BushTrips\nevada" + '\\'
 lnmpln_path_original = r"C:\Users\Moshe Bergman\Documents\Flight Plans\Nevada\Main - VFR Breckenridge Airport (O64) to Mariposa-Yosemite (KMPI).lnmpln"
 lnmpln_path_modified = r"C:\Users\Moshe Bergman\Documents\Flight Plans\Nevada\Main - VFR Breckenridge Airport (O64) to Mariposa-Yosemite (KMPI) - Modified.lnmpln"
-html_path = r"C:\Users\Moshe Bergman\Documents\Flight Plans\Nevada\nevada.html"
+html_path = r"C:\Users\Moshe Bergman\Dropbox\Flight Plans\nevada.html"
 
+def fix_lat_lon(str):
+    return str[1:] + str[0]
 
-with open(bush_trip_path + "en-US.locPak", "r") as file_contents:
+with open(path_to_locPak, "r") as file_contents:
     locPak = json.load(file_contents)
     strings = locPak['LocalisationPackage']['Strings']
     # ordered_strings = list(strings.keys())
@@ -27,23 +28,25 @@ with open(bush_trip_path + "en-US.locPak", "r") as file_contents:
     poi_to_names = {}
 
     config = ConfigParser()
-    config.read(inner_bush_trip_path + path_to_flt)
+    config.read(path_to_flt)
     for key, value in config.items('ATC_ActiveFlightPlan.0'):
         if key.startswith("waypoint"):
             parts = value.split(",")
             poi = parts[1].strip()
+            lat = dms2dec(fix_lat_lon(parts[5].strip()))
+            lon = dms2dec(fix_lat_lon(parts[6].strip()))
 
             if poi not in poi_to_names:
                 poi_to_names[poi] = []
 
             if parts[3].strip().startswith("TT"):
                 tt_key = parts[3].strip().split(":")[1]
-                poi_to_names[poi].append(strings[tt_key])
+                poi_to_names[poi].append({"str": strings[tt_key], "lat": lat, "lon": lon})
             else:
-                poi_to_names[poi].append(parts[3].strip())
+                poi_to_names[poi].append({"str": parts[3].strip(), "lat": lat, "lon": lon})
 
 
-    desc = minidom.parse(inner_bush_trip_path + path_to_xml)
+    desc = minidom.parse(path_to_xml)
 
     sublegs = []
 
@@ -54,16 +57,29 @@ with open(bush_trip_path + "en-US.locPak", "r") as file_contents:
         output += "<table>"
         for subleg in leg.getElementsByTagName('SubLeg'):
             poi = subleg.getElementsByTagName("ATCWaypointStart")[0].attributes['id'].value
-            poi_name = poi_to_names[poi].pop(0)
+            poi_info = poi_to_names[poi].pop(0)
+            poi_name = poi_info["str"]
+            poi_lat = poi_info["lat"]
+            poi_lon = poi_info["lon"]
             comment = strings[subleg.getElementsByTagName("SimBase.Descr")[0].firstChild.nodeValue.split(":")[1]]
             sublegs.append({"name": poi_name, "comment": comment})
-            output += "<tr><td>" + poi + "</td><td>" + poi_name + "</td><td>" + comment + "</td></tr>"
+            output += "<tr><td style='white-space: nowrap'>"
+            output += poi
+            output += " (<a href='http://maps.google.com/maps/place/" + str(poi_lat) + "+" + str(poi_lon) + "' target='_blank'>Gmaps</a>,"
+            output += " <a href='https://skyvector.com/?ll=" + str(poi_lat) + "," + str(poi_lon) + "&chart=301&zoom=2' target='_blank'>SkyVector</a>)"
+            output += "</td><td>" + poi_name + "</td><td>" + comment + "</td></tr>"
 
             image_tag = subleg.getElementsByTagName("ImagePath")[0].firstChild.nodeValue.strip()
             if len(image_tag) > 0:
                 airport_poi = subleg.getElementsByTagName("ATCWaypointEnd")[0].attributes['id'].value
-                airport_name = poi_to_names[airport_poi][0]
-                output += "<tr><td>" + airport_poi + "</td><td>" + airport_name + "</td><td><img src='file://" + inner_bush_trip_path + image_tag + "' width='50%'></td></tr>"
+                airport_info = poi_to_names[airport_poi][0]
+                airport_name = airport_info["str"]
+                airport_lat = airport_info["lat"]
+                airport_lon = airport_info["lon"]
+                output += "<tr><td>" + airport_poi
+                output += " (<a href='http://maps.google.com/maps/place/" + str(airport_lat) + "+" + str(airport_lon) + "' target='_blank'>Gmaps</a>,"
+                output += " <a href='https://skyvector.com/?ll=" + str(airport_lat) + "," + str(airport_lon) + "&chart=301&zoom=2' target='_blank'>SkyVector</a>)"
+                output += "</td><td>" + airport_name + "</td><td><img src='file://" + image_path_prefix + image_tag + "' width='50%'></td></tr>"
 
 
         output += "</table>"
